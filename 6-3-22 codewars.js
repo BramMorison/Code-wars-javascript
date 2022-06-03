b549bd079eb7a3987ed3cc9b3897bf9c1f8ed770
Bram <101137357+BramMorison@users.noreply.github.com>
2022-06-03 03:11:59 -0400

    Add files via upload

---
+++ 6-3-22 codewars.js
@@ -0,0 +1,36 @@
+Sentence Smash
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+
+
+smash = function (words) {
+    return words.join(" ");
+  };
+
+
+  // Smash Words
+function smash (words) {
+    "use strict";
+    return words.join(' ');
+};
+
+
+// Smash Words
+const smash = words => words.join(' ');
+
+
+let smash = words => words.join(" ");
+
+
+// Smash Words
+function smash (words) {
+    "use strict";
+    var smashed = '';
+    for(var i = 0; i<words.length; i++) {
+      smashed += words[i];
+      if(i!=words.length-1) {
+        smashed += ' ';
+      }
+    }
+    return smashed;
+};
